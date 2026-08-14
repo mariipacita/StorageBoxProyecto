29977f2cf72ab5b93c659c4faa5d9101c5870dfe
iva <ivallen84@gmail.com>
2026-08-14 13:50:46 -0600

    Se crea clase EspaciosDeAlmacenamientos
    se establecen variables, metodos getter y setter y una clase enum

---
+++ src/EspaciosDeAlmacenamiento/espacio.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EspaciosDeAlmacenamiento;
+
+/**
+ *
+ * @author PC
+ */
+public class espacio {
+    private int numEspacios;
+    private double tamaño;
+     TipoEspacioEnum Tipoespacio;
+     private int PrecioMensual;
+
+    public espacio(int numEspacios, double tamaño, TipoEspacioEnum Tipoespacio, int PrecioMensual) {
+        this.numEspacios = numEspacios;
+        this.tamaño = tamaño;
+        this.Tipoespacio = Tipoespacio;
+        this.PrecioMensual = PrecioMensual;
+    }
+  
+     
+     
+     
+     
+  
+
+    public int getNumEspacios() {
+        return numEspacios;
+    }
+
+    public double getTamaño() {
+        
+        
+        return tamaño;
+    }
+
+    public TipoEspacioEnum getTipoespacio() {
+        return Tipoespacio;
+    }
+
+    public int getPrecioMensual() {
+        return PrecioMensual;
+    }
+
+    public void setNumEspacios(int numEspacios) {
+        this.numEspacios = numEspacios;
+    }
+
+    public void setTamaño(double tamaño) {
+        this.tamaño = tamaño;
+    }
+
+    public void setPrecioMensual(int PrecioMensual) {
+        this.PrecioMensual = PrecioMensual;
+    }
+     
+     
+    
+    public void EstadoOcupacion(){
+               
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
