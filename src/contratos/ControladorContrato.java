b4acc7629b135ea791d0a800de963c5a3822c388
maripacita <mariichavarriajerez29@gmail.com>
2026-08-17 01:04:58 -0600

    Metodo en Controlador Contrato
    Se agrega un metodo más en clase contrato

--- src/contratos/ControladorContrato.java
+++ src/contratos/ControladorContrato.java
@@ -3,11 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package contratos;
-
+import contratos.Contrato;
 /**
  *
  * @author marii
  */
 public class ControladorContrato {
-    
+   private Contrato contrato;
+   
+   public ControladorContrato(Contrato contrato) {
+        this.contrato = contrato;
+    }
+   
+   public void activarContrato() {
+    contrato.activarContrato();
+}
+
+   public void finalizarContrato() {
+    contrato.finalizarContrato();
+}
+
+   public void cancelarContrato() {
+    contrato.cancelarContrato();
+}
+   public void calcularCosto(int cantidadDias) {
+    contrato.calcularCosto(cantidadDias);
+}
+   
 }
