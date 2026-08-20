3366e6a05c8eb4dceda9842e66b7f24bd945e62d
Sharys29 <sharysceciliano@gmail.com>
2026-08-20 14:16:09 -0600

    control Servicio y empleado
    Se cambia controladores a la clases controlador

--- src/Controlador/ControlEmpleado.java
+++ src/Controlador/ControlEmpleado.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package empleados;
+package Controlador;
 
+import empleados.Empleado;
 import java.util.ArrayList;
 import nulls.Puesto;
 
