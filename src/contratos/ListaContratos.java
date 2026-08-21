4caf3d4848c689ae55bafe694cd668bd5587ac51
maripacita <mariichavarriajerez29@gmail.com>
2026-08-21 14:30:27 -0600

    Clase Lista de Contratos
    Se crea la clase de lista de contratos

---
+++ src/contratos/ListaContratos.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package contratos;
+import StorageBox.KeyDynamicsList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ *
+ * @author marii
+ */
+public class ListaContratos implements KeyDynamicsList<Contrato, Integer> {
+
+    private HashMap<Integer, Contrato> contratos;
+
+    public ListaContratos() {
+        this.contratos = new HashMap<>();
+    }
+
+    public Contrato get(Integer numeroContrato) {
+
+        if (!contratos.containsKey(numeroContrato)) {
+            return null;
+        }
+        return contratos.get(numeroContrato);
+    }
+
+    public boolean remove(Integer numeroContrato) {
+        return contratos.remove(numeroContrato) != null;
+    }
+
+    public boolean add(Contrato item) {
+        if (contratos.containsKey(item.getNumeroContrato())) {
+            return false;
+        }
+
+        return contratos.put(item.getNumeroContrato(), item) == null;
+    }
+
+    public Iterator getAll() {
+
+        if (contratos.isEmpty()) {
+            return null;
+        }
+        return contratos.values().iterator();
+    }
+
+    public int size() {
+        return contratos.size();
+    }
+
+    public boolean isEmpty() {
+        return contratos.isEmpty();
+    }
+
+}
