0672e475f2aac3f23ae0df144f7daf4679347435
maripacita <mariichavarriajerez29@gmail.com>
2026-08-21 20:22:28 -0600

    Enum Puesto Empleado
    Se cambia el enum de puesto empleado al paquete empleados y se borra el paquete donde estaba

--- src/empleados/PuestoEmpleado.java
+++ src/empleados/PuestoEmpleado.java
@@ -2,29 +2,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
-package nulls;
+package empleados;
 
 /**
  *
- * @author sharys
+ * @author marii
  */
-public enum Puesto {
-    
+public enum PuestoEmpleado {
     ADMINISTRADOR(950000),
     RECEPCIONISTA(700000),
     ENCARGADO_BODEGA(650000),
     MANTENIMIENTO(600000),
     OPERARIO_CARGA(575000);
-    
+
     private double salario;
-    
-    Puesto(double salario){
-    this.salario = salario;
-}
+
+    PuestoEmpleado(double salario) {
+        this.salario = salario;
+    }
+
     public double getSalario() {
         return salario;
     }
-    
-    
-    
 }
