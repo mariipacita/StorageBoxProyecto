ae3ad30549a30b056aacb8ef7444845f2c96ca9d
maripacita <mariichavarriajerez29@gmail.com>
2026-08-27 01:48:19 -0600

    Excepcion nueva
    Se crea una nueva excepcion personalizada

---
+++ src/excepciones/EspacioOcupadoException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones;
+
+/**
+ *
+ * @author marii
+ */
+public class EspacioOcupadoException extends Exception {
+    public EspacioOcupadoException(String mensaje) {
+        super(mensaje);
+    }
+}
